0035a7cea84afda7b773873d05cad5f3b025928c
Wang Guangzu <wgz199154@hotmail.com>
2023-03-03 09:33:58 +0800

    initial commit

---
+++ src/main/java/ibf/ssf/assessment/ssf_assessment_shoppingcart/SsfAssessmentShoppingcartApplication.java
@@ -0,0 +1,13 @@
+package ibf.ssf.assessment.ssf_assessment_shoppingcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsfAssessmentShoppingcartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsfAssessmentShoppingcartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ibf/ssf/assessment/ssf_assessment_shoppingcart/SsfAssessmentShoppingcartApplicationTests.java
@@ -0,0 +1,13 @@
+package ibf.ssf.assessment.ssf_assessment_shoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsfAssessmentShoppingcartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
